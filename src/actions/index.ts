671d23b7bcdd545517f3664cabe40e5b47d54cf9
gunnerr91 <blissvortex@hotmail.com>
2020-01-05 13:27:58 +0000

    added todos action and reducer and updated the root reducer

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,14 +1,26 @@
 import axios from "axios";
 import { Dispatch } from "redux";
+import { ActionTypes } from "./types";
 
-const url = "";
+export interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+export interface FetchTodosAction {
+  type: ActionTypes.fetchTodos;
+  payload: Todo[];
+}
+
+const url = "https://jsonplaceholder.typicode.com/todos";
 
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
-    const response = await axios.get(url);
+    const response = await axios.get<Todo[]>(url);
 
-    dispatch({
-      type: "FETCH_TODOS",
+    dispatch<FetchTodosAction>({
+      type: ActionTypes.fetchTodos,
       payload: response.data
     });
   };
---
+++ src/reducers/todos.ts
@@ -0,0 +1,12 @@
+import { Todo, FetchTodosAction } from "./../actions/index";
+import { ActionTypes } from "../actions/types";
+
+export const todosReducer = (state: Todo[] = [], action: FetchTodosAction) => {
+  switch (action.type) {
+    case ActionTypes.fetchTodos:
+      return action.payload;
+
+    default:
+      return state;
+  }
+};
