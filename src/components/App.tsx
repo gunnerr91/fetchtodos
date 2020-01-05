c35902055645f6533f96aa53189eea980b0f7394
gunnerr91 <blissvortex@hotmail.com>
2020-01-05 13:00:50 +0000

    added reducers and components in seperate folders

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export class App extends React.Component {
+  render() {
+    return <div>such hello, much world</div>;
+  }
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import App from "./components/App";
+import { reducers } from "./reducers";
 
-interface AppProps {
-  color: string;
-}
+const store = createStore(reducers, applyMiddleware(thunk));
 
-interface AppState {
-  counter: number;
-}
-
-class App extends React.Component<AppProps, AppState> {
-  constructor(props: AppProps) {
-    super(props);
-
-    this.state = { counter: 0 };
-  }
-
-  increment = (): void => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
-
-  decrement = (): void => {
-    this.setState({ counter: this.state.counter - 1 });
-  };
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.increment}>Increment</button>
-        <button onClick={this.decrement}>Decrement</button>
-        {this.state.counter}
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App color="red" />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
